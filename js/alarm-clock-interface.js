2766d58b050416322f6ae2b74227d5f70ce74cc6
Andrew Fisher and Garrett Boggs <garrett@>
2016-10-11 15:29:29 -0700

    add extra features: sound and more

--- js/alarm-clock-interface.js
+++ js/alarm-clock-interface.js
@@ -1,23 +1,57 @@
 var Alarm = require("./../js/alarm-clock.js").alarmModule;
 var setTime = moment();
 var testAlarm = new Alarm();
-function reloadTime(){
-  setInterval(function(){
-    $("#time").text(moment().format('MMMM do YYY, h:mm:ss a'));
-    testAlarm.checkTime(moment(),setTime);
-  }, 1000)
-}
+var snd = new Audio("images/Quack.mp3");
+var duckSound = false;
+
 
 $(document).ready(function(){
+  $("#duck").hide();
+  function reloadTime(){
+    setInterval(function(){
+      $("#time").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+      if(setTime) {
+        if(testAlarm.checkTime(moment(),setTime)){
+          $("#duck").show();
+          duckSound=true;
+        }
+        if(duckSound){
+          snd.play();
+        }
+      }
+    }, 1000)
+  }
+
   reloadTime();
 
   $("#set-button").submit(function(event){
     event.preventDefault();
+    $("#duck").hide();
+    duckSound=false;
     var hour = parseInt($("#hours").val());
     var minute = parseInt($("#minutes").val());
     var second = parseInt($("#seconds").val());
-    setTime = testAlarm.setAlarm(hour,minute,second);
-
+    var type = $('input[name="meridian"]:checked').val();
+    console.log(type);
+    setTime = testAlarm.setAlarm(hour,minute,second,type);
+    console.log(moment().hours());
     $("#check").text("hour: "+hour+" minute: "+minute + " seconds: " + second);
   })
+
+  $("#stop-alarm").submit(function(event){
+    event.preventDefault();
+    setTime = null;
+    $("#duck").hide();
+    $("#check").hide();
+    duckSound=false;
+  });
+
+  $("#duck").click(function(event){
+    event.preventDefault();
+    setTime = testAlarm.setAlarm(moment().hours(),moment().minutes(),moment().seconds()+5);
+    $("#duck").hide();
+
+     duckSound=false;
+  });
+
 });
--- js/alarm-clock.js
+++ js/alarm-clock.js
@@ -8,14 +8,31 @@ Alarm.prototype.viewTime = function() {
 
 };
 
-Alarm.prototype.setAlarm = function (h , m , s) {
+Alarm.prototype.setAlarm = function (h , m , s,type) {
+    console.log("initial: " + s);
+  if(type === 'pm'){
+    h+=12;
+  }
+
+  if(s > 59){
+    m += 1;
+    s -= 59;
+  }
+  if(m > 59){
+    h += 1;
+    m -= 59;
+  }
+
+  console.log(h);
+
   var setTime = moment({hour: h, minute: m, seconds: s});
+
   return setTime;
 };
 
 Alarm.prototype.checkTime = function(current , set) {
-  if(current.seconds() === set.seconds()){
-    alert("Ring Ring!");
+  if(current.seconds() === set.seconds() && current.minutes() === set.minutes() && current.hours() === set.hours()){
+    return true;
   }
 };
 
